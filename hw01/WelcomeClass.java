71bd9204330fd8e1b452b5464afdd0acd2b916e4
tomschneible <tomschneible@gmail.com>
2015-01-23 22:51:36 +0000

    HW1 WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+// Tom Schneible
+// CSE 002 Spring2015
+// HW 1 Welcome Class
+
+//  defining class
+    public class WelcomeClass{
+
+    //  main method
+        public static void main(String[] args) {
+        
+        // Printing the statement
+        // Note that spacing is important to ensure figure is properly aligned 
+            System.out.println("   -----------");
+            System.out.println("   | WELCOME |");
+            System.out.println("   -----------");
+            System.out.println("  ^  ^  ^  ^  ^  ^ ");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+            System.out.println("<-T--J--S--4--1--7->");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+            System.out.println("  v  v  v  v  v  v ");
+            System.out.println("This summer I worked as a cashier at KMart, a subsidiary");
+            System.out.println("of Sears Holdings Corporation. I began work on my birthday");
+       
+       // We see that we need to use double backslashes to prevent illegal escape errors     
+    }
+            
+}
+        
+
