680b081728b94b2d6365cd44a4f2a879f04fc228
tomschneible <tomschneible@gmail.com>
2015-01-23 16:56:46 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,33 @@
+// Tom Schneible
+// Lab 1 Hello World Program
+
+//       Compile the program 
+//       javac HelloWorld.java
+        
+//       Run the program
+//       java HelloWord//
+        
+
+//       Define the class
+public class HelloWorld{
+
+//       Add the main method
+    public static void main(String[] args) {
+    
+//       Print the Statement with a box around it
+
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hellow World 2015!! #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+    }
+}
+        
+        
+        
+        
